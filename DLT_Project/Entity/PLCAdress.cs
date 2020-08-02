e317233bdcc87b278a51b064335a555ee1e8c382
Administrator <Administrator@USER-9O3IVP96N8>
2020-08-02 20:06:55 +0800

    Modify

--- DLT_Project/Entity/PLCAdress.cs
+++ DLT_Project/Entity/PLCAdress.cs
@@ -9,5 +9,42 @@ namespace DLT_Project.Entity
     public class PLCAdress
     {
         public static string Barcode { get { return "D4500"; } }
+
+        /// <summary>
+        /// 1:加热；0:停止
+        /// </summary>
+        public static string HeaterSignal { get { return "D5000"; } }
+        /// <summary>
+        /// 回馈PLC信号
+        /// </summary>
+        public static string HeaterBack { get { return "D5001"; } }
+        /// <summary>
+        /// 读取电阻信号 1:读取 0:不读取
+        /// </summary>
+        public static string ResSignal { get { return "D5002"; } }
+        /// <summary>
+        /// 电阻值回写PLC
+        /// </summary>
+        public static string ResDataBack { get { return "D5003"; } }
+        /// <summary>
+        /// PLC传检测OK，通知PC存储检测记录
+        /// </summary>
+        public static string OverSignal { get { return "D5005"; } }
+        /// <summary>
+        /// PC反馈存储结果
+        /// </summary>
+        public static string OverBack { get { return "D5006"; } }
+
+        public static string LHeater { get { return "D4000"; } }
+        public static string LSBROn { get { return "D4002"; } }
+        public static string LSafety { get { return "D4004"; } }
+        public static string LWalkIn { get { return "D4006"; } }
+        public static string LBukle { get { return "D4008"; } }
+
+        public static string RHeater { get { return "D4100"; } }
+        public static string RSBROn { get { return "D4102"; } }
+        public static string RSafety { get { return "D4104"; } }
+        public static string RWalkIn { get { return "D4106"; } }
+        public static string RBukle { get { return "D4108"; } }
     }
 }
--- DLT_Project/MainWindow.xaml.cs
+++ DLT_Project/MainWindow.xaml.cs
@@ -29,14 +29,20 @@ namespace DLT_Project
     {
 
         private ConfigData config;
+        private MesInfo mesInfo;
         private MesDataOpService mesDataOpService;
         private ResDataOpService resDataOpService;
         private DispatcherTimer ShowTimer;
         private bool sPort = false;
         private bool mData = false;
         private bool mPlc = false;
-        private static BitmapImage IFalse = new BitmapImage(new Uri("/Images/01.png", UriKind.Relative));
-        private static BitmapImage ITrue = new BitmapImage(new Uri("/Images/02.png", UriKind.Relative));
+        private static BitmapImage IFalse = new BitmapImage(new Uri("/Static/01.png", UriKind.Relative));
+        private static BitmapImage ITrue = new BitmapImage(new Uri("/Static/02.png", UriKind.Relative));
+        private DispatcherTimer timer;
+        private bool dataMark = false;
+        private int over = 0;
+        private string markBarcode = null;
+
 
         public MainWindow()
         {
@@ -44,10 +50,22 @@ namespace DLT_Project
 
             Init();
 
+            //连接ok
+            if (sPort && mData && mPlc)
+            {
+                CycleDataRead();
+            }
+
             DispatcherTimer dispatcherTimer = new DispatcherTimer();
             dispatcherTimer.Tick += (s, e) =>
             {
-                resDataOpService.ReadData();
+                // start 存储
+                if (over == 1 && dataMark)
+                {
+                    bool t = mesDataOpService.DataInsert(mesInfo);
+                }
+
+
             };
             dispatcherTimer.Interval = TimeSpan.FromMilliseconds(2000);
             dispatcherTimer.Start();
@@ -60,7 +78,7 @@ namespace DLT_Project
         /// </summary>
         private void LoadJsonData()
         {
-            using (var sr = File.OpenText("C:\\config\\config2.json"))
+            using (var sr = File.OpenText("C:\\config\\DLTConfig.json"))
             {
                 string JsonStr = sr.ReadToEnd();
                 if (config == null)
@@ -86,12 +104,78 @@ namespace DLT_Project
 
             mData = mesDataOpService.GetConnection();
             sPort = resDataOpService.GetConnection();
+            //PLC connection
+            //Lin connection
 
             PLCImage.Source = (mPlc ? ITrue : IFalse);
             DataImage.Source = (mData ? ITrue : IFalse);
             PortImage.Source = (sPort ? ITrue : IFalse);
+        }
 
+        private void CycleDataRead()
+        {
 
+            timer = new DispatcherTimer();
+            timer.Tick += (s, e) =>
+            {
+                //读取BarCode
+                string barCode = null;
+                //初始化状态
+                if (barCode != null && !barCode.Equals("") && !barCode.Equals(markBarcode))
+                {
+                    markBarcode = barCode;
+                    dataMark = false;
+                }
+                //判断型号
+
+                #region 读取Heater信号
+                int heater = 1;
+                switch (heater)
+                {
+                    // 0 发送停止加热 00 00 00 00 00 00 00 00
+                    case 0:
+                        break;
+                    //1 发送驱动加热 00 00 ?? ?? 00 00 00 00 // ??->E3  L  R
+                    case 1:
+                        break;
+                    default: break;
+                }
+                #endregion
+
+                #region 读取Res信号
+                int res = 0;
+                if (res == 1)
+                {
+                    resDataOpService.ReadData();
+                    //write PLC data
+
+                }
+                #endregion
+
+                #region 读取存储信号
+                over = 1;
+
+                if (over == 1 && !dataMark)
+                {
+                    dataMark = true;
+                    // 读取测试数据资料
+                    mesInfo = new MesInfo();
+                    // 存储MES数据
+                    bool t = mesDataOpService.DataInsert(mesInfo);
+                    if (!t) // 存储失败继续下一次存储直到成功
+                    {
+                        dataMark = false;
+                    }else
+                    {
+                        // 回写PLC存储状态 1:成功
+                    }
+
+                }
+                #endregion
+
+            };
+            timer.Interval = TimeSpan.FromMilliseconds(10);
+            timer.Start();
         }
 
         private void ShowTimer1(object sender, EventArgs e)
@@ -130,16 +214,19 @@ namespace DLT_Project
             mesDataOpService = null;
             resDataOpService = null;
             config = null;
-            Init();
-        }
-
-        private void PlcData()
-        {
-
-            if (true)
+            if(timer != null && timer.IsEnabled)
             {
+                timer.Stop();
+                timer = null;
+            }
 
+            Init();
+            //连接ok
+            if (sPort && mData && mPlc)
+            {
+                CycleDataRead();
             }
         }
+
     }
 }
